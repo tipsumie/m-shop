7533e748e215938f992acbaa7efa27a8afb42905
tipsumie <tipsukon.prm@gmail>
2023-02-27 02:39:12 +0700

    fix margin of loading

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { LoadingOutlined } from '@ant-design/icons';
+import { Row, Layout } from 'antd';
 import styled from 'styled-components';
+
+const { Content } = Layout;
 
 const Loading = () => {
   return (
     <LoadingContainer>
-      <LoadingOutlined style={{ fontSize: 70, fontWeight: 'bold' }} />
+      <LoadingOutlined
+        style={{ fontSize: 70, fontWeight: 'bold', marginTop: '90px' }}
+      />
       <h3>Loading.....</h3>
     </LoadingContainer>
   );
